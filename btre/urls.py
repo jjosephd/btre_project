c1c6d0f6ef18c6a0b2571e56a2268c5e63925b86
jjosephd <jjosephd804@gmail.com>
2025-02-08 11:29:45 -0500

    Updated with comments

--- btre/urls.py
+++ btre/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+# URL patterns for the project
 urlpatterns = [
+    # Include URLs from the 'pages' app
     path('', include('pages.urls')),
+    # Include URLs from the 'listings' app
     path('listings/', include('listings.urls')),
+    # Admin site URLs
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
